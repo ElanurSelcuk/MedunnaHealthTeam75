31fd82f8de52528801d3eba415bff494f9aa4e9e
ElanurSelcuk <elanur.kibis@gmail.com>
2022-03-02 22:29:00 -0500

    first try

---
+++ src/test/java/StepDefs/RegistrationPage01StepDefs.java
@@ -0,0 +1,36 @@
+package StepDefs;
+
+import io.cucumber.java.en.Given;
+
+public class RegistrationPage01StepDefs {
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+
+    @Given("user clicks on login dropdown")
+    public void user_clicks_on_login_dropdown() {
+
+    }
+
+    @Given("user clicks on register button")
+    public void user_clicks_on_register_button() {
+
+    }
+
+    @Given("user sends ssn")
+    public void user_sends_ssn() {
+
+    }
+
+    @Given("user sends firstname")
+    public void user_sends_firstname() {
+
+    }
+
+    @Given("user sends lastname")
+    public void user_sends_lastname() {
+
+    }
+
+}
