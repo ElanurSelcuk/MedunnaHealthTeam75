e6fe5c6890e38e4470cae66d60037c801019cdea
tugceacariskender <tugceacariskender@gmail.com>
2022-03-03 16:02:31 -0500

    my first codes

---
+++ src/test/java/stepdefinitons/RegistrationPage02StepDefs.java
@@ -0,0 +1,42 @@
+package stepdefinitons;
+
+import io.cucumber.java.en.Given;
+
+public class RegistrationPage02StepDefs {
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+    @Given("user clik on login dropdown")
+    public void user_clik_on_login_dropdown() {
+
+    }
+    @Given("user clik on register button")
+    public void user_clik_on_register_button() {
+
+    }
+    @Given("user sends firstname")
+    public void user_sends_firstname() {
+
+    }
+    @Given("user sends lastname")
+    public void user_sends_lastname() {
+
+    }
+    @Given("user sends username")
+    public void user_sends_username() {
+
+    }
+    @Given("user send email")
+    public void user_send_email() {
+
+
+    }
+
+
+
+
+
+
+}
